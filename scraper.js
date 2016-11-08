5017198a7cd9648ce8310cb6c06b4b8f52257a29
Mraleksa <a.gribanovskiy@gmail.com>
2016-11-09 01:35:44 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -41,11 +41,11 @@ function fetchPage(url, callback) {
 
 function run(db) {
 	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
+	fetchPage("http://oligarh.org.ua", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
+		var elements = $("body").each(function () {
 			var value = $(this).text().trim();
 			updateRow(db, value);
 		});
